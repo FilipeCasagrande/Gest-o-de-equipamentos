9093171eba30d50f25e76f45ec3c21035589fc8f
Filipe <Filipecasagrande1@gmail.com>
2025-05-05 22:31:58 -0300

    Add project files.

---
+++ Gestão de equipamentos/Equipamento.cs
@@ -0,0 +1,13 @@
+﻿namespace Gestão_de_equipamentos
+{
+    public class Equipamento
+    {
+        public int id;
+        public string nome;
+        public decimal preco;
+        public string fabricante;
+        public string serie;
+        public DateTime dataFabricacao;
+
+    }
+}
---
+++ Gestão de equipamentos/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Gestão_de_equipamentos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TelaEquipamento telaEquipamento = new TelaEquipamento();
+
+            while (true)
+            {
+                char opcaoEscolhida = telaEquipamento.ApresentarMenu();
+
+                if (opcaoEscolhida == 'S')
+                    break;
+
+                switch (opcaoEscolhida)
+                {
+                    case '1':
+                        telaEquipamento.CadastrarRegistro();
+                        break;
+                    case '2':
+                        telaEquipamento.VisualizarRegistros(true);
+                        break;
+                    case '3':
+                        telaEquipamento.editarEquipamento();
+                        break;
+                    case '4':
+                        telaEquipamento.ExcluirRegistro();
+                        break;
+                }
+
+            }
+        }
+    }
+}
